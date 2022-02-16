cf3e26823179593d2c991578a43bdb3c5c4df766
kaiop <kaioplandel@gmail.com>
2022-02-16 10:32:06 -0300

    Initial commit

---
+++ app/src/main/java/com/plandel/spinner/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.plandel.spinner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.plandel.spinner.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val customList = listOf("First","Second","Third","Fourth")
+        val adapter = ArrayAdapter<String> (this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,customList)
+
+        binding.spinner.adapter = adapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@MainActivity,"You selected ${parent?.getItemAtPosition(position).toString()}",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
+}
